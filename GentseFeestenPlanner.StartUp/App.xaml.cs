4bf09e7afe29947b8235528b3e3bcaec7df5e55b
KenGod88 <ken.godfroid@student.hogent.be>
2023-12-16 12:21:22 +0100

    Initial commit

---
+++ GentseFeestenPlanner.StartUp/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace GentseFeestenPlanner.StartUp
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
