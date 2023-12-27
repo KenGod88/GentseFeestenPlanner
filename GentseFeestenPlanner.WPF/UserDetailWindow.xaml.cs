a92622d119aad5b3e48b57476a3c34ccdd99892b
KenGod88 <ken.godfroid@student.hogent.be>
2023-12-27 14:51:55 +0100

    Update UserDetailWindow.xaml.cs

--- GentseFeestenPlanner.WPF/UserDetailWindow.xaml.cs
+++ GentseFeestenPlanner.WPF/UserDetailWindow.xaml.cs
@@ -1,20 +1,7 @@
-﻿using GentseFeestenPlanner.Domain;
-using GentseFeestenPlanner.Domain.DTO;
+﻿using GentseFeestenPlanner.Domain.DTO;
 using GentseFeestenPlanner.Domain.Model;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
-using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace GentseFeestenPlanner.WPF
 {
@@ -23,14 +10,21 @@ namespace GentseFeestenPlanner.WPF
     /// </summary>
     public partial class UserDetailWindow : Window
     {
+        //EventHandlers
+        public event EventHandler<DateTime> DayPlanSelected;
+
+        public event EventHandler<EventDTO> EventSelected;
+
+        public event EventHandler AddDayPlan;
+
         //Event properties
         private List<EventDTO> _events;
-        public List<EventDTO> Events { get => _events ; set => DayPlanDetailsListBox.ItemsSource = value; }
-        public event EventHandler<DateTime> DayPlanSelected;
-        public event EventHandler<EventDTO> EventSelected;
+
+        public List<EventDTO> Events { get => _events; set => DayPlanDetailsListBox.ItemsSource = value; }
 
         //User properties
         public int UserId { get; set; }
+
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string FullName { get => UserNameTitle.Text; set => UserNameTitle.Text = value; }
@@ -38,28 +32,16 @@ namespace GentseFeestenPlanner.WPF
 
         //DayPlan properties
         public List<DayPlan> DayPlans { get; set; }
-        private List<string> _DayPlanDates;  
-        public List<string> DayPlanDates { get => _DayPlanDates ; set => ExistingPlansListBox.ItemsSource = value; }
+
+        private List<string> _DayPlanDates;
+        public List<string> DayPlanDates { get => _DayPlanDates; set => ExistingPlansListBox.ItemsSource = value; }
 
         public UserDetailWindow()
         {
             InitializeComponent();
-
-
         }
 
-        private void ExistingPlansListBox_MouseDoubleClick(object sender, MouseButtonEventArgs e)
-        {
-            if (ExistingPlansListBox.SelectedItem is string selectedDateString && DateTime.TryParse(selectedDateString, out DateTime selectedDate))
-            {
-                DayPlanSelected?.Invoke(this, selectedDate);
-            }
-            else
-            {
-                MessageBox.Show("Please select a valid day plan date.", "No Dayplan Selected", MessageBoxButton.OK, MessageBoxImage.Information);
-            }
-
-        }
+        
 
         private void DayPlanDetailsListBox_MouseDoubleClick(object sender, MouseButtonEventArgs e)
         {
@@ -71,7 +53,23 @@ namespace GentseFeestenPlanner.WPF
             {
                 MessageBox.Show("Please select a valid event.", "No Event Selected", MessageBoxButton.OK, MessageBoxImage.Information);
             }
+        }
 
+        private void AddPlanButton_Click(object sender, RoutedEventArgs e)
+        {
+            AddDayPlan?.Invoke(this, EventArgs.Empty);
+        }
+
+        private void ExistingPlansListBox_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            if (ExistingPlansListBox.SelectedItem is string selectedDateString && DateTime.TryParse(selectedDateString, out DateTime selectedDate))
+            {
+                DayPlanSelected?.Invoke(this, selectedDate);
+            }
+            else
+            {
+                MessageBox.Show("Please select a valid day plan date.", "No Dayplan Selected", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
         }
     }
 }
