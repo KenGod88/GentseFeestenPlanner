1c831677679a0ad62f08a6e1fa5c87d05a9e7240
KenGod88 <ken.godfroid@student.hogent.be>
2024-01-01 20:35:24 +0100

    Added some logic to the domain tests (not Working)

---
+++ GentseFeestenPlanner.Tests/DomainRulesTests.cs
@@ -0,0 +1,38 @@
+﻿using GentseFeestenPlanner.Domain;
+using GentseFeestenPlanner.Domain.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GentseFeestenPlanner.Tests
+{
+    public class DomainRulesTests
+    {
+
+        [Theory]
+        [InlineData("123", true)]
+        [InlineData("456", false)]
+        public void CheckIfEventAlreadyPlanned_Throws_WhenEventAlreadyPlanned(string eventIdToAdd, bool shouldThrow)
+        {
+            // Arrange
+            var eventToAdd = new Event { EventId = eventIdToAdd };
+            var currentDayPlan = new DayPlan();
+            currentDayPlan.Events.Add(new Event { EventId = "123" }); // Add an event with Id "123"
+
+            // Act & Assert
+            if (shouldThrow)
+            {
+                Assert.Throws<InvalidOperationException>(() => currentDayPlan.CheckIfEventAlreadyPlanned(eventToAdd, currentDayPlan));
+            }
+            else
+            {
+                currentDayPlan.CheckIfEventAlreadyPlanned(eventToAdd, currentDayPlan); // Should not throw
+            }
+        }
+
+
+
+    }
+}
